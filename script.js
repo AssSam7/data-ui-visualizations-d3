b894e9f13adb5a2a3a50ad14d510932a4b8ce118
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2020-08-23 19:11:48 +0530

    Adding update() function

--- script.js
+++ script.js
@@ -24,7 +24,65 @@ const graph = svg
 const xAxisGroup = graph
   .append("g")
   .attr("transform", `translate(0, ${graphHeight})`);
+
 const yAxisGroup = graph.append("g");
+
+// Linear scale
+const y = d3.scaleLinear().range([graphHeight, 0]);
+
+// Band scale
+const x = d3
+  .scaleBand()
+  .range([0, graphHeight])
+  .paddingInner(0.2)
+  .paddingOuter(0.2);
+
+// Creating the Axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y);
+
+yAxis.ticks(3).tickFormat((d) => d + " orders");
+
+// Update x-axis text
+xAxisGroup
+  .selectAll("text")
+  .attr("transform", "rotate(-40)")
+  .attr("text-anchor", "end")
+  .attr("fill", "crimson");
+
+// Update function
+const update = (data) => {
+  // 1. Updating linear and band scale on data update
+  y.domain([0, d3.max(data, (d) => d.orders)]);
+  x.domain(data.map((item) => item.name));
+
+  // 2. Joining data to rects
+  const rects = graph.selectAll("rect").data(data);
+
+  // 3. Remove exit selection
+  rects.exit().remove();
+
+  // 4. Update current shapes in DOM
+  rects
+    .attr("width", x.bandwidth)
+    .attr("height", (d) => y(d.orders))
+    .attr("fill", "crimsom")
+    .attr("x", (d) => x(d.name));
+
+  // 5. Append items from enter selection
+  rects
+    .enter()
+    .append("rect")
+    .attr("width", x.bandwidth)
+    .attr("height", (d) => graphHeight - y(d.orders))
+    .attr("fill", "crimson")
+    .attr("x", (d) => x(d.name))
+    .attr("y", (d) => y(d.orders));
+
+  // Call the axes
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+};
 
 db.collection("dishes")
   .get()
@@ -35,52 +93,5 @@ db.collection("dishes")
       data.push(doc.data());
     });
 
-    console.log(data);
-
-    // Joining data to rects
-    const rects = graph.selectAll("rect").data(data);
-    // Linear scale
-    const y = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, (d) => d.orders)])
-      .range([graphHeight, 0]);
-
-    // Band scale
-    const x = d3
-      .scaleBand()
-      .domain(data.map((item) => item.name))
-      .range([0, graphHeight])
-      .paddingInner(0.2)
-      .paddingOuter(0.2);
-
-    // rects
-    //   .attr("width", x.bandwidth)
-    //   .attr("height", (d) => y(d.orders))
-    //   .attr("fill", "crimsom")
-    //   .attr("x", (d) => x(d.name));
-
-    // Append items from enter selection
-    rects
-      .enter()
-      .append("rect")
-      .attr("width", x.bandwidth)
-      .attr("height", (d) => graphHeight - y(d.orders))
-      .attr("fill", "crimson")
-      .attr("x", (d) => x(d.name))
-      .attr("y", (d) => y(d.orders));
-
-    // Creating Axes
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisLeft(y);
-
-    yAxis.ticks(3).tickFormat((d) => d + " orders");
-
-    xAxisGroup.call(xAxis);
-    yAxisGroup.call(yAxis);
-
-    xAxisGroup
-      .selectAll("text")
-      .attr("transform", "rotate(-40)")
-      .attr("text-anchor", "end")
-      .attr("fill", "crimson");
+    update(data);
   });
