4223f5959bd6dae345c9a59c5fcd16351c394bfd
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2020-08-24 20:39:02 +0530

    Adding real-time data update from firestore

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const svg = d3
   .attr("width", 600)
   .attr("height", 600);
 
-// Creating margins and groups
+// Creating margins and dimensions
 const margin = {
   top: 20,
   right: 20,
@@ -21,6 +21,7 @@ const graph = svg
   .attr("height", graphHeight)
   .attr("transform", `translate(${margin.left},${margin.top})`);
 
+// Creating axes group
 const xAxisGroup = graph
   .append("g")
   .attr("transform", `translate(0, ${graphHeight})`);
@@ -33,41 +34,36 @@ const y = d3.scaleLinear().range([graphHeight, 0]);
 // Band scale
 const x = d3
   .scaleBand()
-  .range([0, graphHeight])
+  .range([0, graphWidth])
   .paddingInner(0.2)
   .paddingOuter(0.2);
 
 // Creating the Axes
 const xAxis = d3.axisBottom(x);
-const yAxis = d3.axisLeft(y);
-
-yAxis.ticks(3).tickFormat((d) => d + " orders");
-
-// Update x-axis text
-xAxisGroup
-  .selectAll("text")
-  .attr("transform", "rotate(-40)")
-  .attr("text-anchor", "end")
-  .attr("fill", "crimson");
+const yAxis = d3
+  .axisLeft(y)
+  .ticks(3)
+  .tickFormat((d) => d + " orders");
 
 // Update function
 const update = (data) => {
-  // 1. Updating linear and band scale on data update
+  // 1. Updating linear and band scale domains on data update
   y.domain([0, d3.max(data, (d) => d.orders)]);
   x.domain(data.map((item) => item.name));
 
   // 2. Joining data to rects
   const rects = graph.selectAll("rect").data(data);
 
-  // 3. Remove exit selection
+  // 3. Remove elements from the Exit selection
   rects.exit().remove();
 
   // 4. Update current shapes in DOM
   rects
     .attr("width", x.bandwidth)
-    .attr("height", (d) => y(d.orders))
-    .attr("fill", "crimsom")
-    .attr("x", (d) => x(d.name));
+    .attr("height", (d) => graphHeight - y(d.orders))
+    .attr("fill", "crimson")
+    .attr("x", (d) => x(d.name))
+    .attr("y", (d) => y(d.orders));
 
   // 5. Append items from enter selection
   rects
@@ -82,16 +78,36 @@ const update = (data) => {
   // Call the axes
   xAxisGroup.call(xAxis);
   yAxisGroup.call(yAxis);
+
+  xAxisGroup
+    .selectAll("text")
+    .attr("fill", "crimson")
+    .attr("transform", "rotate(-40)")
+    .attr("text-anchor", "end");
 };
 
-db.collection("dishes")
-  .get()
-  .then((res) => {
-    let data = [];
+// Global data array
+let data = [];
 
-    res.docs.forEach((doc) => {
-      data.push(doc.data());
-    });
+db.collection("dishes").onSnapshot((res) => {
+  res.docChanges().forEach((change) => {
+    const doc = { ...change.doc.data(), id: change.doc.id };
 
-    update(data);
+    switch (change.type) {
+      case "added":
+        data.push(doc);
+        break;
+      case "modified":
+        const index = data.findIndex((item) => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case "removed":
+        data = data.filter((item) => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
   });
+
+  update(data);
+});
